97bd97314712646d341a4fd8fc384df35d4f4aec
Graco2005 <luisgraconeto@gmail.com>
2026-09-09 14:33:57 -0300

    commit 09/09/26

---
+++ 02-python-intermediario-funcoes-modulos-etc/aula37.py
@@ -0,0 +1,13 @@
+# Recarregando módulos, importlib e singleton
+
+import importlib
+
+import aula37_m
+
+print(aula37_m.variavel)
+
+for i in range(10):
+    importlib.reload(aula37_m)
+    print(i)
+
+print('Fim')
---
+++ 02-python-intermediario-funcoes-modulos-etc/aula38.py
@@ -0,0 +1,10 @@
+# Introdução aos packages (pacotes) em Python
+
+from sys import path
+
+from aula38_package.modulo import soma, variavel
+
+# print(*path, sep='\n')
+
+print(soma(1, 2))
+print(variavel)
