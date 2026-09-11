f8dec0e4b2219e2bbde2509acfd017fe79fcce6d
Graco2005 <luisgraconeto@gmail.com>
2026-09-11 02:07:09 -0300

    commit 11/09/26

---
+++ 02-python-intermediario-funcoes-modulos-etc/aula38_package/__init__.py
@@ -0,0 +1,5 @@
+# Arquivo de inicialização __init__
+# Funciona para inicializar pacotes em Python
+
+from aula38_package.modulo import *
+from aula38_package.modulo_b import *
---
+++ 02-python-intermediario-funcoes-modulos-etc/aula39.py
@@ -0,0 +1,8 @@
+# Arquivo de inicialização __init__
+# Funciona para inicializar pacotes em Python
+
+from aula38_package import fala_oi, soma
+
+print(soma(1, 2))
+
+fala_oi()
